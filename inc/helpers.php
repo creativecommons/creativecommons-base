f54ffb9e56c6a1b016c8ce941a16bbb4acaacec6
Hugo Solar <hugo.solar@gmail.com>
2020-03-25 17:35:06 -0300

    fix spaces in helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -7,57 +7,57 @@
  * @author Felipe Lavín <felipe@yukei.net>
  * */
 function do_excerpt($post, $args=null){
-	$params = wp_parse_args($args, array(
-		'length' => 255,
-		'echo' => false,
-		'strict' => false,
-		'wrap' => null,
-		'wrap_id' => null,
-		'wrap_class' => 'entry-excerpt',
-		'hellip' => false,
-		'append' => null
-	));
-	$out = $wrap = '';
-	if ( $params['wrap'] ) {
-		$wrap_id = $params['wrap_id'] ? ' id="'. esc_attr( $params['wrap_id'] ) .'"' : null;
-		$wrap_class = $params['wrap_class'] ? ' class="'. esc_attr( $params['wrap_class'] ) .'"' : null;
-		$wrap = '<'. $params['wrap'] . $wrap_id . $wrap_class .'>';
-	}
-	if ( is_string($post) ) {
-		$excerpt = strip_shortcodes($post);
-		$excerpt = strip_tags($excerpt);
-		if ( strlen($excerpt) > $params['length'] ) {
-			$excerpt = smart_substr($excerpt, $params['length']);
-			if ( $params['hellip'] ) $excerpt .= ' '. $params['hellip'];
-		}
-		if ( $params['append'] ) $excerpt .= ' '. $params['append'];
-		$out .= apply_filters('the_excerpt', $excerpt);
-	} elseif ( is_object($post) ) {
-		if ( isset($post->post_excerpt) && !empty($post->post_excerpt) ) {
-			if ( $params['strict'] && strlen($post->post_excerpt) > $params['length'] ) {
-				$buff = smart_substr($post->post_excerpt, $params['length']);
-				if ($params['hellip'] ) $buff .= ' '. $params['hellip'];
-				if ( $params['append'] ) $buff .= ' '. $params['append'];
-				$out .= apply_filters('the_excerpt', $buff);
-			} else {
-				if ( $params['append'] ) $post->post_excerpt .= ' '. $params['append'];
-				$out .= apply_filters('the_excerpt', $post->post_excerpt);
-			}
-		} elseif ( isset($post->post_content) && !empty($post->post_content) ) {
-			return do_excerpt($post->post_content, $params);
-		}
-	}
+  $params = wp_parse_args($args, array(
+    'length' => 255,
+    'echo' => false,
+    'strict' => false,
+    'wrap' => null,
+    'wrap_id' => null,
+    'wrap_class' => 'entry-excerpt',
+    'hellip' => false,
+    'append' => null
+  ));
+  $out = $wrap = '';
+  if ( $params['wrap'] ) {
+    $wrap_id = $params['wrap_id'] ? ' id="'. esc_attr( $params['wrap_id'] ) .'"' : null;
+    $wrap_class = $params['wrap_class'] ? ' class="'. esc_attr( $params['wrap_class'] ) .'"' : null;
+    $wrap = '<'. $params['wrap'] . $wrap_id . $wrap_class .'>';
+  }
+  if ( is_string($post) ) {
+    $excerpt = strip_shortcodes($post);
+    $excerpt = strip_tags($excerpt);
+    if ( strlen($excerpt) > $params['length'] ) {
+      $excerpt = smart_substr($excerpt, $params['length']);
+      if ( $params['hellip'] ) $excerpt .= ' '. $params['hellip'];
+    }
+    if ( $params['append'] ) $excerpt .= ' '. $params['append'];
+    $out .= apply_filters('the_excerpt', $excerpt);
+  } elseif ( is_object($post) ) {
+    if ( isset($post->post_excerpt) && !empty($post->post_excerpt) ) {
+      if ( $params['strict'] && strlen($post->post_excerpt) > $params['length'] ) {
+        $buff = smart_substr($post->post_excerpt, $params['length']);
+        if ($params['hellip'] ) $buff .= ' '. $params['hellip'];
+        if ( $params['append'] ) $buff .= ' '. $params['append'];
+        $out .= apply_filters('the_excerpt', $buff);
+      } else {
+        if ( $params['append'] ) $post->post_excerpt .= ' '. $params['append'];
+        $out .= apply_filters('the_excerpt', $post->post_excerpt);
+      }
+    } elseif ( isset($post->post_content) && !empty($post->post_content) ) {
+      return do_excerpt($post->post_content, $params);
+    }
+  }
 
-	if ( !$out ) return false;
+  if ( !$out ) return false;
 
-	if ( $out ) {
-		if ( $params['wrap'] ) {
-		    $wrap .= $out . '</'. $params['wrap'] .'>';
-		    $out = $wrap;
-		}
-		if( $params['echo'] ) echo $out;
-		else return $out;
-	}
+  if ( $out ) {
+    if ( $params['wrap'] ) {
+        $wrap .= $out . '</'. $params['wrap'] .'>';
+        $out = $wrap;
+    }
+    if( $params['echo'] ) echo $out;
+    else return $out;
+  }
 }
 
 /**
@@ -68,16 +68,16 @@ function do_excerpt($post, $args=null){
  * @author Basilio Cáceres <bcaceres@ayerviernes.com>
  */
 function smart_substr($str,$n,$hellip=true){
-	if ( strlen($str) > $n ) {
-		$out = substr( strip_tags($str), 0, $n );
-		$out = explode(" ",$out);
-		array_pop( $out );
-		$out = implode(" ",$out);
-		if ( $hellip ) $out .= ' [&hellip;]';
-	} else {
-		$out = $str;
-	}
-	return $out;
+  if ( strlen($str) > $n ) {
+    $out = substr( strip_tags($str), 0, $n );
+    $out = explode(" ",$out);
+    array_pop( $out );
+    $out = implode(" ",$out);
+    if ( $hellip ) $out .= ' [&hellip;]';
+  } else {
+    $out = $str;
+  }
+  return $out;
 }
 
 /**
@@ -86,91 +86,91 @@ function smart_substr($str,$n,$hellip=true){
  */
 
 class videos {
-	/**  
-	*  Extract and return video id from youtbe & vimeo videos
-	*	  @param string url : Video url ( whether youtube or vimeo )
-	*/
-	static function get_video_embed( $url, $width = '100%', $height = 450 ) {
-		if ( strstr( $url, 'youtube' ) ){
-			$parse_url = parse_url( $url );
+  /**  
+  *  Extract and return video id from youtbe & vimeo videos
+  *	  @param string url : Video url ( whether youtube or vimeo )
+  */
+  static function get_video_embed( $url, $width = '100%', $height = 450 ) {
+    if ( strstr( $url, 'youtube' ) ){
+      $parse_url = parse_url( $url );
       $output = array();
-			parse_str( $parse_url['query'], $output );
-			$id = $output['v'];
-			$iframe = '<iframe width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$id.'" frameborder="0" allowfullscreen></iframe>';
-			return $iframe;
-		}
+      parse_str( $parse_url['query'], $output );
+      $id = $output['v'];
+      $iframe = '<iframe width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$id.'" frameborder="0" allowfullscreen></iframe>';
+      return $iframe;
+    }
     if ( strstr( $url, 'youtu.be' ) ) {
       $parse_url = parse_url( $url );
       $id = array_pop( explode( '/', $parse_url ) );
       $iframe = '<iframe width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$id.'" frameborder="0" allowfullscreen></iframe>';
       return $iframe;
     }
-		if ( strstr( $url, 'vimeo' ) ) {
-			if ( !empty( $url ) ) {
-				$id = array_pop( explode( '/', $url ) );
-				$iframe='<iframe src="http://player.vimeo.com/video/'.$id.'" width="'.$width.'" height="'.$height.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-				return $iframe;
-			}
-		}
-		return false;
+    if ( strstr( $url, 'vimeo' ) ) {
+      if ( !empty( $url ) ) {
+        $id = array_pop( explode( '/', $url ) );
+        $iframe='<iframe src="http://player.vimeo.com/video/'.$id.'" width="'.$width.'" height="'.$height.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+        return $iframe;
+      }
+    }
+    return false;
 
-	}
+  }
   /**
    * return a contextual class to add in layout whether the video is youtbe or vimeo
    *
    * @param [string] $url: the url of the video 
    * @return string class name
    */
-	static function get_video_class( $url ) {
-		if (strstr($url,'youtube')){
-			return 'youtube-video';
-		}
-		if (strstr($url,'vimeo')){
-			return 'vimeo-video';
-		}
-	}
-	/**
-	* Extract and return video id from youtbe & vimeo videos
-	* @param string url : Video url
-	* @param string size : small, full
+  static function get_video_class( $url ) {
+    if (strstr($url,'youtube')){
+      return 'youtube-video';
+    }
+    if (strstr($url,'vimeo')){
+      return 'vimeo-video';
+    }
+  }
+  /**
+  * Extract and return video id from youtbe & vimeo videos
+  * @param string url : Video url
+  * @param string size : small, full
   * @return string video thumb url
-	*/
-	static function get_video_thumb( $url, $size = 'full' ) {
-		if ( strstr( $url, 'youtube' ) ){
-			$parse_url = parse_url( $url );
-			parse_str( $parse_url['query'] );
-			$id = $v;
-			$def_size = ( $size == 'small' )? 1:0;
-			$thumb = 'http://img.youtube.com/vi/'.$id.'/'.$def_size.'.jpg';
-			return $thumb;
-		}
+  */
+  static function get_video_thumb( $url, $size = 'full' ) {
+    if ( strstr( $url, 'youtube' ) ){
+      $parse_url = parse_url( $url );
+      parse_str( $parse_url['query'] );
+      $id = $v;
+      $def_size = ( $size == 'small' )? 1:0;
+      $thumb = 'http://img.youtube.com/vi/'.$id.'/'.$def_size.'.jpg';
+      return $thumb;
+    }
     if (strstr($url,'youtu.be')) {
       $parse_url = parse_url( $url );
       $id = array_pop( explode( '/', $parse_url ) );
       $def_size = ( $size == 'small' )? 1:0;
-			$thumb = 'http://img.youtube.com/vi/'.$id.'/'.$def_size.'.jpg';
-			return $thumb;
+      $thumb = 'http://img.youtube.com/vi/'.$id.'/'.$def_size.'.jpg';
+      return $thumb;
     }
-		if ( strstr( $url, 'vimeo' ) ) {
-			$thumb = '';
-			if ( !empty( $url ) ) {
-				if ( false === ( $thumb = get_transient( 'vimeo_'.$id ) ) ) {
-				$id = array_pop( explode( '/', $url) ); 
-				$request = new WP_Http;
-				$result = $request->request( 'http://vimeo.com/api/v2/video/'.$id.'.php' , $args );
-				if ( !is_wp_error( $result ) ) {
-					$body = maybe_unserialize( $result['body'] );
-					if ( $size == 'medium' ) 
-						$thumb = $body[0]['thumbnail_medium'];
-					else
-						$thumb = $body[0]['thumbnail_large'];
-					set_transient( 'vimeo_'.$id,$thumb,60*60*24 );
-				}
-			}
-				return $thumb;
-			}
-		}
-		return false;
+    if ( strstr( $url, 'vimeo' ) ) {
+      $thumb = '';
+      if ( !empty( $url ) ) {
+        if ( false === ( $thumb = get_transient( 'vimeo_'.$id ) ) ) {
+        $id = array_pop( explode( '/', $url) ); 
+        $request = new WP_Http;
+        $result = $request->request( 'http://vimeo.com/api/v2/video/'.$id.'.php' , $args );
+        if ( !is_wp_error( $result ) ) {
+          $body = maybe_unserialize( $result['body'] );
+          if ( $size == 'medium' ) 
+            $thumb = $body[0]['thumbnail_medium'];
+          else
+            $thumb = $body[0]['thumbnail_large'];
+          set_transient( 'vimeo_'.$id,$thumb,60*60*24 );
+        }
+      }
+        return $thumb;
+      }
+    }
+    return false;
 
-	}
+  }
 }
