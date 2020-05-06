03b176040e022cc90f9046ae75aa4856441123bd
Hugo Solar <hugo.solar@gmail.com>
2020-05-06 15:54:24 -0400

    space fix

---
+++ template-no-sidebar.php
@@ -0,0 +1,33 @@
+<?php
+	/** Template name: No sidebar */
+
+	get_header();
+	the_post();
+	$post_format = get_post_format();
+?>
+<section class="main-content">
+	<header class="single-header">
+		<div class="container">
+			<div class="columns is-centered">
+				<div class="column is-8">
+					<?php get_template_part( 'inc/partials/entry/page', 'header' ); ?>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+		<div class="columns is-centered">
+			<div class="column is-8">
+				<section class="entry-page-content">
+					<div class="text-format body-big">
+						<?php the_content(); ?>
+					</div>
+					<footer class="entry-footer">
+						<?php get_template_part( 'inc/partials/entry/entry', 'footer' ); ?>
+					</footer>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
