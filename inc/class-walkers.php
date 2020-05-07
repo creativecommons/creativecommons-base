bc953c6b08768367ae95b41c811373637e3e34b2
Hugo Solar <hugo.solar@gmail.com>
2020-05-07 08:42:39 -0400

    top & side navigation styles

--- inc/class-walkers.php
+++ inc/class-walkers.php
@@ -18,7 +18,10 @@ class Navwalker extends Walker_Nav_Menu {
 		$output .= "<div class='navbar-dropdown'>";
 	}
 	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		$li_classes   = 'navbar-item ' . $item->title;
+		$classes   = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+		$li_classes   = 'navbar-item ' . $item->post_name . ' ' . $class_names;
 		$has_children = $args->walker->has_children;
 		$li_classes  .= $has_children ? ' has-dropdown is-hoverable' : '';
 		if ( $has_children ) {
