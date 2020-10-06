328db2d1cafa8f79cc7797e6ab6fbebfb644709a
JackieBinya <jacquelinebinya@gmail.com>
2020-10-06 16:31:38 +0200

    Removes default styles from the docs vue template

--- docs/src/templates/Doc.vue
+++ docs/src/templates/Doc.vue
@@ -36,25 +36,3 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-/deep/ > p {
-	opacity: 0.8;
-}
-
-/deep/ > h2 {
-	padding-top: 100px;
-	margin-top: -80px;
-
-	@include respond-above(md) {
-		font-size: 2rem;
-	}
-}
-
-/deep/ > p > img {
-	max-width: 100%;
-}
-
-.markdown {
-	padding-bottom: 50vh;
-}
-</style>
