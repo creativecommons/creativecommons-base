af4094a30a5d2b9c9c2c3dd9a587cbd83dd66b49
Brylie Christopher Oxley <brylie@creativecommons.org>
2021-08-04 19:16:14 +0300

    Remove layout divs

--- template-no-sidebar.php
+++ template-no-sidebar.php
@@ -7,26 +7,18 @@
 <section class="main-content">
 	<header class="single-header">
 		<div class="container">
-			<div class="columns is-centered">
-				<div class="column is-8">
-					<?php get_template_part( 'inc/partials/entry/page', 'header' ); ?>
-				</div>
-			</div>
+			<?php get_template_part( 'inc/partials/entry/page', 'header' ); ?>
 		</div>
 	</header>
-	<div class="container">
-		<div class="columns is-centered">
-			<div class="column is-8">
-				<section class="entry-page-content">
-					<div class="text-format body-big">
-						<?php the_content(); ?>
-					</div>
-					<footer class="entry-footer">
-						<?php get_template_part( 'inc/partials/entry/page', 'footer' ); ?>
-					</footer>
-				</section>
-			</div>
+
+	<section class="entry-page-content">
+		<div class="text-format body-big">
+			<?php the_content(); ?>
 		</div>
-	</div>
+		<footer class="entry-footer">
+			<?php get_template_part( 'inc/partials/entry/page', 'footer' ); ?>
+		</footer>
+	</section>
+
 </section>
 <?php get_footer(); ?>
