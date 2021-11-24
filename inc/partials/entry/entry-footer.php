4fd0a1fcc54f4e70ee8dae0944cb0d4f3b3c7112
Muluh MG Godson <godsonmuluh@gmail.com>
2021-11-24 21:53:16 +0100

    add whatsapp share and adjust styling

--- inc/partials/entry/entry-footer.php
+++ inc/partials/entry/entry-footer.php
@@ -1,6 +1,6 @@
 <hr>
 <div class="columns leveled">
-	<div class="column is-8">
+	<div class="column">
 		<div class="entry-meta">
 			<div class="terms-list the-categories margin-vertical-normal">
 				<h6>Categories</h6>
@@ -15,9 +15,12 @@
 	<div class="column is-2">
 		<div class="share-entry margin-vertical-normal">
 			<h6>Share</h6>
-			<a href="<?php echo CC_Site::social_share( 'facebook', get_the_ID() ); ?>" class="share facebook"><i class="icon facebook colored"></i></a>
-			<a href="<?php echo CC_Site::social_share( 'twitter', get_the_ID() ); ?>" class="share twitter"><i class="icon twitter colored"></i></a>
-			<a href="<?php echo CC_Site::social_share( 'linkedin', get_the_ID() ); ?>" class="share linkedin"><i class="icon linkedin colored"></i></a>
+			<div class="is-flex is-3">
+				<a href="<?php echo CC_Site::social_share( 'facebook', get_the_ID() ); ?>" class="share column facebook"><i class="icon facebook colored"></i></a>
+				<a href="<?php echo CC_Site::social_share( 'twitter', get_the_ID() ); ?>" class="share column twitter"><i class="icon twitter colored"></i></a>
+				<a href="<?php echo CC_Site::social_share( 'whatsapp', get_the_ID() ); ?>" class="share column twitter"><i class="icon whatsapp colored"></i></a>
+				<a href="<?php echo CC_Site::social_share( 'linkedin', get_the_ID() ); ?>" class="share column linkedin"><i class="icon linkedin colored"></i></a>
+			</div>
 		</div>
 	</div>
 </div>
