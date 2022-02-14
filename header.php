3cd86b6025f873d9859225fa8352669f108f8937
Muluh MG Godson <godsonmuluh@gmail.com>
2022-02-14 22:09:33 +0100

    [header.php] upgrade to new vocabulary-fonts

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-	<link rel="stylesheet" href="https://unpkg.com/@creativecommons/fonts@2020.9.3/css/fonts.css">
+	<link rel="stylesheet" href="https://unpkg.com/@creativecommons/vocabulary-fonts@0.x.x/dist/css/fonts.css">
 	<link rel="stylesheet" href="https://unpkg.com/@creativecommons/vocabulary-styles@0.x.x/dist/css/vocabulary-styles.css">
 	<title><?php wp_title('|'); ?></title>
 	<?php wp_head(); ?>
