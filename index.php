93456382c81ec0356a20177dea4baa37a1944928
Muluh MG Godson <godsonmuluh@gmail.com>
2022-02-14 09:31:12 +0100

    [index.php] remove breadcrumb code

--- index.php
+++ index.php
@@ -8,11 +8,6 @@
 			<div class="columns is-centered">
 				<div class="column">
 					<h2><?php echo CC_Site::page_title(); ?></h2>
-					<?php
-					if ( function_exists( 'yoast_breadcrumb' ) ) {
-						yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
-					}
-					?>
 				</div>
 			</div>
 		</div>
