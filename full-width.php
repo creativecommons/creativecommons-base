074ed95774110a2642e3cdee9511ae63cd507dc7
Muluh MG Godson <godsonmuluh@gmail.com>
2022-03-03 21:27:33 +0100

    add new template for the homepage and other fullwidth pages

---
+++ full-width.php
@@ -0,0 +1,18 @@
+<?php
+/* Template name: Full Width */
+	get_header();
+	the_post();
+?>
+
+<section class="main-content">
+	<section class="entry-page-content">
+		<div class="content">
+			<?php the_content(); ?>
+		</div>
+		<footer class="entry-footer">
+			<?php get_template_part( 'inc/partials/entry/page', 'footer' ); ?>
+		</footer>
+	</section>
+
+</section>
+<?php get_footer(); ?>
